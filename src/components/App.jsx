686a7df0294c5529f426413f3c0c0d22aab76049
irinasokolenko <127216070+irinasokolenko@users.noreply.github.com>
2023-09-22 13:59:18 +0300

    hw041

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,64 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { useState } from 'react';
+
+import { Section } from './Section/Section';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Statistics } from './Statistics/Statistics';
+import { Notification } from './Notification/Notification';
+
+export class App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const leaveFeedback = ({ target: { name } }) => {
+    switch (name) {
+      case 'good':
+        setGood(prevGood => prevGood + 1);
+        break;
+        case 'neutral':
+          setNeutral(prevNeutral => prevNeutral + 1);
+        break;
+        case 'bad':
+          setBad(prevBad => prevBad + 1);
+        break;
+        default:
+          console.log(`Type fedback name - ${name} is not`);
+     }
+  };
+
+  const countTotalFeedback = () => {
+    return Object.values(obt).reduce((total, curr) => (total += curr));
+  };
+
+   const countPositiveFeedbackPercentage = totalFeedback => {
+        if (totalFeedback > 0) return Math.round((good / totalFeedback) * 100);
+    return 0;
+  };
+    const totalFeedback = countTotalFeedback({good, neutral, bad});
+    const positivePercentage = countPositiveFeedbackPercentage(totalFeedback);
+    return (
+      <>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={[good, neutral, bad]}
+            leaveFeedback={leaveFeedback}
+          />
+        </Section>
+
+        <Section title="Statistics">
+          {totalFeedback ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={totalFeedback}
+              positivePercentage={positivePercentage}
+            />
+          ) : (
+            <Notification message="There is no feedback" />
+          )}
+        </Section>
+      </>
+    );
+  };
+
